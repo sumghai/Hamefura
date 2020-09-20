b13a4c3f6dd925c407a8119c2d2bbf0f3678099c
Robin Chang <sumdumghai@gmail.com>
2020-09-20 09:02:57 +0800

    (WIP) Implement hooded apparel

---
+++ Source/CompProperties_ApparelDrawAttachedHeadgear.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Hamefura
+{
+    class CompProperties_ApparelDrawAttachedHeadgear : CompProperties
+    {
+        public GraphicData headgearGraphicData;
+
+        public CompProperties_ApparelDrawAttachedHeadgear()
+        {
+            compClass = typeof(CompApparelDrawAttachedHeadgear);
+        }
+    }
+}
---
+++ Source/HamefuraMod.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Hamefura
+{
+    public class HamefuraMod : Mod
+    {
+        public HamefuraMod(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony("com.Hamefura.patches");
+            harmony.PatchAll();
+        }
+    }
+}
