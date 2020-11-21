ad4cb881dad4bd8fc66a00ef354bde7cf22c2d38
Robin Chang <sumdumghai@gmail.com>
2020-11-21 17:22:26 +0800

    Prevent stuff color from tinting apparel UI icons

---
+++ Source/CompApparelIgnoreStuffColor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace Hamefura
+{
+    public class CompApparelIgnoreStuffColor : CompColorable
+    {
+        public override void Initialize(CompProperties props)
+        {
+            base.Initialize(props);
+            CompColorable comp = parent.GetComp<CompColorable>();
+            if (comp != null && !comp.active)
+            {
+                parent.GetComp<CompColorable>().active = true;
+            }
+            parent.Notify_ColorChanged();
+        }
+    }
+}
