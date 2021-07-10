e714ac81ac5981fd05f7a42ede371890f4a20aca
Robin Chang <sumdumghai@gmail.com>
2021-07-10 20:38:06 +0800

    Fix toggleable headgear patch

---
+++ Source/Patches/Harmony_PawnGraphicSet_ResolveApparelGraphics.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace Hamefura.Patches
+{
+    [HarmonyPatch(typeof(PawnGraphicSet), nameof(PawnGraphicSet.ResolveApparelGraphics))]
+    public static class Harmony_PawnGraphicSet_ResolveApparelGraphics
+    {
+        // After resolving a pawn's apparel graphics, we conditionally render any headgear attached to apparel
+        // (e.g. hoods, masks, helmets) depending on the Gizmo state in CompApparelWithAttachedHeadgear
+        static void Postfix(PawnGraphicSet __instance, Pawn ___pawn)
+        {
+            using (List<Apparel>.Enumerator enumerator = ___pawn.apparel.WornApparel.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    ApparelGraphicRecord item;
+                    if (enumerator.Current.TryGetComp<CompApparelWithAttachedHeadgear>() is CompApparelWithAttachedHeadgear comp)
+                    {
+                        Apparel hoodApparel = (Apparel)ThingMaker.MakeThing(comp.Props.attachedHeadgearDef);
+                        ApparelGraphicRecordGetter.TryGetGraphicApparel(hoodApparel, ___pawn.story.bodyType, out ApparelGraphicRecord hoodApparelGraphicRecord);
+
+                        if (comp.isHatOn)
+                        {
+                            ___pawn.Drawer.renderer.graphics.apparelGraphics.Add(hoodApparelGraphicRecord);
+                        }
+                        else
+                        {
+                            ___pawn.Drawer.renderer.graphics.apparelGraphics.RemoveAll(x => x.sourceApparel == hoodApparel);
+                        }
+
+                        GlobalTextureAtlasManager.TryMarkPawnFrameSetDirty(___pawn);
+                    }
+                }
+            }
+        }
+    }
+}
