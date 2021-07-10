08474a044152c65b1b79bbd0b1890b3323cbe419
Robin Chang <sumdumghai@gmail.com>
2021-07-10 21:21:37 +0800

    Improve apparel stuff color ignoring on crafting

---
+++ Source/Patches/Harmony_GenRecipe_PostProcessProduct.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Verse;
+
+namespace Hamefura.Patches
+{
+    [HarmonyPatch(typeof(GenRecipe), nameof(GenRecipe.PostProcessProduct))]
+    public static class Harmony_GenRecipe_PostProcessProduct
+    {
+        static void Prefix(ref Thing product)
+        {
+            CompApparelIgnoreStuffColor compApparelIgnoreStuffColor = product.TryGetComp<CompApparelIgnoreStuffColor>();
+
+            if (compApparelIgnoreStuffColor != null)
+            {
+                product.SetColor(Color.white);
+            }
+        }
+    }
+
+}
